869979e6b1de6ff7bb95e18b3777f3235e67f97f
VertigoSHI <554964845@qq.com>
2022-01-02 09:35:05 +0800

    DB模块轮子搭建

--- MySpiders/util/DB/DBUtil.py
+++ MySpiders/util/DB/DBUtil.py
@@ -9,8 +9,10 @@ config = ConfigParser()
 config.read(DBConfigPath)
 
 
-# 接口类 定义基本操作
 class BaseDBUtil:
+    """
+    基本DB操作工具类
+    """
     DB_name = None
 
     @classmethod
@@ -18,11 +20,11 @@ class BaseDBUtil:
         raise NotImplementedError
 
     @classmethod
-    def create_table_with_schema(cls, tableName, schemaList: {}):
+    def create_table_with_schema(cls, DBName, tableName, schemaList: {}):
         raise NotImplementedError
 
     @classmethod
-    def create_table_with_class(cls, tableName, data_class: object):
+    def create_table_with_class(cls, DBName, tableName, data_class: object):
         raise NotImplementedError
 
     @classmethod
@@ -34,7 +36,7 @@ class BaseDBUtil:
         raise NotImplementedError
 
     @classmethod
-    def read(cls, DBName, tableName) -> []:
+    def read(cls, DBName, tableName):
         raise NotImplementedError
 
     @classmethod
@@ -43,20 +45,25 @@ class BaseDBUtil:
 
 
 class DBOperationResult:
+    """
+    操作结果
+    """
     is_success: bool = None
     msg: str = None
+    data: iter = None
 
-    def __init__(self, result: bool, msg: str):
+    def __init__(self, result: bool, msg: str, data: []):
         self.is_success = result
         self.msg = msg
+        self.data = data
 
     @staticmethod
-    def success():
-        return DBOperationResult(True, "")
+    def success(data):
+        return DBOperationResult(True, "", data)
 
     @staticmethod
     def fail(msg: str):
-        return DBOperationResult(False, msg)
+        return DBOperationResult(False, msg, None)
 
 
 class MysqlDBUtil(BaseDBUtil):
@@ -72,29 +79,40 @@ class MysqlDBUtil(BaseDBUtil):
     # 创建库
     @classmethod
     def create_DB(cls, DBName):
-        pass
+        cls.execute('create database if not exists' + DBName + ';')
 
     @classmethod
     def execute(cls, expression: str):
         try:
             cls.cursor.execute(expression)
-        except err.MySQLError as e:
+        except err.MySQLError as exception:
             cls.db.rollback()
-            return DBOperationResult.fail(str(e))
+            return DBOperationResult.fail(str(exception))
         else:
             cls.db.commit()
-            return DBOperationResult.success()
+            return DBOperationResult.success(cls.cursor.fetchall())
 
     @classmethod
-    def read(cls, DBName, tableName) -> []:
-        pass
+    def read(cls, DBName, tableName):
+        sql = "use" + DBName + ";\n" + "select * from " + tableName + ";\n"
+        return cls.execute(sql)
 
     @classmethod
-    def create_table_with_schema(cls, tableName, schemaList: {}):
-        pass
+    def create_table_with_schema(cls, DBName, tableName, schemaMap: {}):
 
+        data = ""
+        for schema in schemaMap:
+            data = data + schema + " " + schemaMap[schema] + ",\n"
+        sql = "use " + DBName + ";" \
+                                "create table " + tableName + "(" + data + ");"
+        return cls.execute(sql)
+
+    # todo
     @classmethod
-    def create_table_with_class(cls, tableName, data_class: object):
+    def create_table_with_class(cls, DBName, tableName, data_class: object):
+        schemaMap = {}
+        for attr in data_class.__dict__:
+            pass
         pass
 
     @classmethod
